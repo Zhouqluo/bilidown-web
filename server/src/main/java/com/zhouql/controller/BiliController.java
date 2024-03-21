24ec811d932b9619992878f996333e04d595ae86
zhouql <3279106972@qq.com>
2024-03-21 23:49:26 +0800

    后端接口实现

--- server/src/main/java/com/zhouql/controller/BiliController.java
+++ server/src/main/java/com/zhouql/controller/BiliController.java
@@ -1,6 +1,9 @@
 package com.zhouql.controller;
 
+import cn.hutool.http.HttpUtil;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -10,8 +13,34 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 public class BiliController {
-    @GetMapping("/")
-    public String test() {
-        return "初始化完成";
+    /**
+     * 移动端链接
+     * @param url
+     * @return
+     */
+    @GetMapping("/move")
+    public String getMobileContent(@RequestParam("url") String url) {
+        return HttpUtil.createGet(url).execute().body();
+    }
+
+    /**
+     * 根据bv获取详细信息，其中avid和cid很重要
+     * @param bv
+     * @return
+     */
+    @GetMapping("/av/{bv}")
+    public String getAv(@PathVariable("bv") String bv) {
+        return HttpUtil.createGet("https://api.bilibili.com/x/web-interface/view?bvid=" + bv).execute().body();
+    }
+
+    /**
+     * 根据avid和cid请求下载地址接口，获取视频地址
+     * @param avid
+     * @param cid
+     * @return
+     */
+    @GetMapping("/download/{avid}/{cid}")
+    public String getDownloadUrl(@PathVariable("avid") String avid, @PathVariable("cid") String cid) {
+        return HttpUtil.createGet("https://api.bilibili.com/x/player/playurl?avid=" + avid + "&cid=" + cid + "&qn=80&type=mp4&platform=html5&high_quality=1").execute().body();
     }
 }
